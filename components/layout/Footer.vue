bad4ddb281e1490a2c3eb606f264c0aa75feda2d
wxf <1019072635@qq.com>
2022-12-20 22:10:33 +0800

    feat: 添加layout及header、footer组件

---
+++ components/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    尾部
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    顶部
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
