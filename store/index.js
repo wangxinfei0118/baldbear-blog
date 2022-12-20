e756c85af5e2dfd370e5518324371af76801f232
wxf <1019072635@qq.com>
2022-12-20 22:04:41 +0800

    feat: 添加store，定义user相关state

---
+++ store/index.js
@@ -0,0 +1,18 @@
+const state = () => ({
+  userInfo: null,
+  accessToken: null,
+  refreshToken: null,
+})
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+export default {
+  state,
+  mutations,
+  actions
+}
