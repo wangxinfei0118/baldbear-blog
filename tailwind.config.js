8b6d13615b63b15a342f2f5c5273e9399d1e4faf
wxf <1019072635@qq.com>
2022-12-21 19:58:59 +0800

    feat: tailwin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'home-title': 'rgb(136, 136, 136)',
+        'note-title': 'rgb(0, 153, 255)',
+        'note-label': 'rgb(39, 175, 182)',
+      },
+      fontSize: {
+        small: '12px',
+        medium: '14px',
+        'medium-x': '16px',
+        large: '18px',
+      },
+      borderRadius: {
+        small: '2px',
+        medium: '4px',
+        large: '8px'
+      },
+      lineHeight: {
+        12: '12px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+      },
+    },
   },
   variants: {
     extend: {},
