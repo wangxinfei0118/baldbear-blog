d28af6f946f4ed03af44ea3f45cf37d8240634e8
wxf <1019072635@qq.com>
2022-12-21 15:03:34 +0800

    feat: vuex管理用户状态，跳转到单点登陆系统

--- store/index.js
+++ store/index.js
@@ -5,10 +5,36 @@ const state = () => ({
 })
 
 const mutations = {
+  // 重置用户状态
+  resetUserState(state){
+    state.userInfo = null
+    state.accessToken = null
+    state.refreshToken = null
+  },
 
+  // 更新用户状态
+  updateUserState(state,userData){
+    state.userInfo = userData.userInfo
+    state.accessToken = userData.accessToken
+    state.refreshToken = userData.refreshToken
+  }
 }
 
 const actions = {
+
+  nuxtServerInit({commit}, {app}){
+    const userData = {}
+    userData.userInfo = app.$cookies.get('userInfo')
+    userData.accessToken = app.$cookies.get('accessToken')
+    userData.refreshToken = app.$cookies.get('refreshToken')
+    commit('updateUserState',userData)
+
+  },
+  // 跳转到认证客户端
+  toLoginPage({commit}){
+    commit('resetUserState')
+    window.location.href = `http://localhost:7000?redirectURL=${window.location.href}`
+  },
 
 }
 export default {
