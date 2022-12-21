81023ff9afb80543214af79f37c4dc61058e698c
wxf <1019072635@qq.com>
2022-12-21 20:00:09 +0800

    feat: 首页页面布局及功能

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,112 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        baldbear-blog
-      </h1>
-      <h2 class="subtitle">
-        秃头熊的个人博客
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <!-- 主体内容 -->
+  <div>
+    <div class="background">
+      <img src="@/assets/background.png">
+    </div>
+    <div class="info w-full pt-6">
+      <div class="info-box">
+        <div class="text-large text-home-title">个人简介</div>
+        <div class="mt-3 mb-5 text-home-title text-medium" style="text-indent: 2em">前端开发工程师，2023年毕业于天津工业大学，软件工程专业，大一开始接触网页设计基础，大二期间参与过小组数据可视化项目的前端开发，大三开始系统学习前端，从前端基础，到开发框架，再到前端工程化以及项目的优化部署，期间做过pc、h5、小程序、跨端开发等相关项目，并在2022.6-2022.9参与企业实习，进行企业级项目开发。平时关注github，稀土掘金等社区了解更多优秀的前端技术及项目，对自己进行进一步的提升</div>
+        <div class="text-home-title self-end">
+          github：<a href="https://github.com/wangxinfei0118" target="_blank"><img class="inline-block align-baseline" src="@/assets/github-mark.svg" width="32" height="32"></a>
+          wechat:   <el-popover
+          placement="top-start"
+          width="200"
+          trigger="click">
+          <img src="@/assets/wechat-share.jpg" width="200" >
+          <img slot="reference" src="@/assets/wechat-logo.svg" width="32" height="32" class="inline-block align-baseline">
+        </el-popover>
+        </div>
+      </div>
+    </div>
+    <el-divider></el-divider>
+    <div class="note w-full" >
+      <div style="width: 78%;margin: 0 auto;display: flex;flex-direction: column;align-items: center">
+        <div class="text-large text-home-title" style="margin: 10px 0">我的笔记</div>
+        <el-divider>记录学习中的每一步脚印</el-divider>
+        <div class="note-box" style="width: 100%;display: flex;justify-content: space-between;flex-wrap: wrap">
+          <el-card shadow="hover" style="width: 280px;margin-bottom: 14px" :body-style="{ padding: '0px' }" v-for="i in 6" :key="i">
+            <img class="w-full h-36" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Flmg.jj20.com%2Fup%2Fallimg%2Ftp09%2F210F2130512J47-0-lp.jpg&refer=http%3A%2F%2Flmg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1674108919&t=9531c288f275ec1e9eb3fadc6a07a48d">
+            <div style="padding: 22px">
+              <div class="text-note-title font-semibold" style="margin-bottom: 10px">前端性能优化</div>
+              <div class="text-medium leading-22">
+                出来找大祭祀的价位较低啊我就带我基地啊我觉得价位较低啊我就带我基地啊我觉得
+              </div>
+              <div class="text-note-label text-small" style="margin: 18px 0 ">标签：项目优化</div>
+              <div class="flex text-small">
+                <div class="w-3/5" >2022年12月18日</div>
+                <div class="w-1/5" >110</div>
+                <div class="w-1/5" >2000</div>
+              </div>
+            </div>
+          </el-card>
+          <div class="list" style="width: 280px;" v-for="item in (4 - 6 % 4)" :key="item" v-if="6 % 4 > 0"></div>
+        </div>
+        <el-button v-if="isMore" type="primary" :loading="loading" size="small" round @click="loadMore">{{ loading ? '加载中……' : '点击加载更多' }}</el-button>
+        <el-tag v-else type="danger">没有更多数据了……</el-tag>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
+
+  data(){
+    return{
+      loading:false,
+      isMore:true
+    }
+  },
+  methods:{
+    async loadMore(){
+      this.loading = true
+      this.page.current++
+      const {data} = await this.$getNoteList(this.page)
+      if (data.records && data.records.length>0){
+        this.isMore = true
+        this.noteList = this.noteList.concat(data.records)
+      }
+      else {
+        this.isMore= false
+      }
+      this.loading = false
+    }
+  },
+  async asyncData( {app} ) {
+
+    const page = {current: 1, size: 8}
+    const { data } = await app.$getNoteList(page)
+    return {page, noteList: data.records}
+
+  },
 }
 </script>
 
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+<style scoped>
+.note .el-divider{
+  width: 400px;
 }
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+.note .el-divider__text{
+  color: rgb(19, 194, 194);
+}
+.background{
+  height: calc(100vh - 54px);
+  width: 100%;
+
+}
+.background img{
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+.info-box{
   display: flex;
-  justify-content: center;
+  margin: 0 auto;
+  width: 78%;
+  flex-direction: column;
   align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
