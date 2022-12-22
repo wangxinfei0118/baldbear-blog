981973cf9b5d3bc9dcfe3d0847a83cd9f86ffe1e
wxf <1019072635@qq.com>
2022-12-22 21:47:51 +0800

    feat: 更新note相关请求api

--- api/note.js
+++ api/note.js
@@ -1,6 +1,30 @@
 export default({$axios}, inject) => {
 
-// 文章列表分页接口
-  inject('getNoteList', query => $axios.$post('/note/list',query))
+  // 文章列表分页接口
+  inject('getNoteList', data => $axios.$post('/note/list', data))
+
+  // 查询文章详情
+  inject('getNoteById', id => $axios.$get( `/note/note/${id}` ))
+
+  // 更新文章浏览数
+  inject('updateNoteViewCount', id => $axios.$put(`/note/viewCount/${id}`))
+
+  // 通过文章ID查询所有评论数据接口
+  inject('getCommentListByNoteId', noteId => $axios.$get( `/note/comment/list/${noteId}` ))
+
+  // 新增文章评论
+  inject('addComment', data => $axios.$post( `/note/comment`, data ))
+
+  // 删除评论
+  inject('deleteCommentById', id => $axios.$delete( `/note/comment/${id}` ))
+
+  //获取分类标签
+  inject('getCategoryAndLabel', () => $axios.$get( `/note/label/list` ))
+
+  // 新增文章
+  inject('addNote', data => $axios.$post( `/note/note`, data ))
+
+  // 编辑文章
+  inject('editNote', data => $axios.$put( `/note/note`, data ))
 
 }
