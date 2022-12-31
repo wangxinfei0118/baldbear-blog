4675523805d34bfad2563a479ba911f9dc7c65ac
wxf <1019072635@qq.com>
2022-12-31 19:14:50 +0800

    feat: 新增获取留言api

--- api/common.js
+++ api/common.js
@@ -3,4 +3,6 @@ export default ({ $axios }, inject) => {
   inject('uploadImg', data => $axios.$post( `/common/img/upload`, data ) )
   // 删除图片
   inject('deleteImg', fileUrl => $axios.$delete( `/common/img/delete`, { params: {fileUrl} } ) )
+  // 获取所有留言
+  inject('getMessage', () => $axios.$get( `/common/message` ))
 }
