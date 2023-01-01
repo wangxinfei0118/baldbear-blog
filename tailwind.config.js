f1dffb0799be34cce2e2cd8940f580d7ec4f0010
wxf <1019072635@qq.com>
2023-01-01 23:32:29 +0800

    更新tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,12 @@ module.exports = {
         'home-title': 'rgb(136, 136, 136)',
         'note-title': 'rgb(0, 153, 255)',
         'note-label': 'rgb(39, 175, 182)',
+        'notice' : 'rgb(255,127,80)',
+        'footer-bg' : 'rgb(244, 245, 245)'
       },
       fontSize: {
         small: '12px',
+        'small-x': '13px',
         medium: '14px',
         'medium-x': '16px',
         large: '18px',
