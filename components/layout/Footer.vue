ae6bacdc237750407483f419b40caf66c2a1ed6b
wxf <1019072635@qq.com>
2023-01-02 00:04:21 +0800

    feat: 添加网页底部内容

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    尾部
+  <div class="bg-footer-bg h-20 flex justify-center items-center text-small-x">
+    <div class="mr-2">
+      <a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022030887号</a>
+    </div>
+    <div>
+      联系方式：wangxinfei0118@163.com
+    </div>
   </div>
 </template>
 
