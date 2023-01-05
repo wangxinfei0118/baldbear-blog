8a10f74355d00c3370e46b4cd402f9d39ef00688
wxf <1019072635@qq.com>
2023-01-05 23:48:51 +0800

    fix:

--- pages/label/_id.vue
+++ pages/label/_id.vue
@@ -3,12 +3,9 @@
       <div class="head">
         <div class="background" style="width: 100%;display: flex;flex-direction: column;align-items: center">
           <div class="bg-img"></div>
-          <div class="head-card" style="width: 60%;height: 180px;box-sizing: border-box;padding: 24px 24px 0;color: #fff;background-color: hsla(0,0%,100%,.5);border: 1px solid #f0f0f0;margin-top: -180px;display: flex;flex-direction: column;align-items: center">
-            <div class="title" style="font-size: 32px;margin-bottom: 16px">
+          <div class="head-card" style="width: 60%;height: 180px;box-sizing: border-box;padding: 24px 24px 0;color: #fff;background-color: hsla(0,0%,100%,.5);border: 1px solid #f0f0f0;margin-top: -180px;display: flex;flex-direction: column;align-items: center;justify-content: center">
+            <div class="title -mt-6" style="font-size: 32px">
               {{$route.query.name}}
-            </div>
-            <div class="summary">
-              这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题这是标题
             </div>
           </div>
         </div>
