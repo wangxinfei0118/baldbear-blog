1d5329098876c8bcec08fd437f2feeadd02e4cf7
wxf <1019072635@qq.com>
2023-01-08 20:38:08 +0800

    feat: 安装cross-env，配置环境变量并根据执行环境获取认证客户端地址

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,12 @@
-
 export default {
   mode: 'universal',
+  env: {
+    // 认证客户端url process.env.authURL
+    authURL: process.env.NODE_ENV === 'dev' ? '//localhost:7000' : '//login.baldbear.cn'
+  },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
     title: '秃头熊的个人博客',
     meta: [
@@ -11,69 +14,59 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '秃头熊的前端知识库' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
-    ]
+    link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
   },
   router: {
     middleware: '404'
   },
   /*
-  ** Customize the progress-bar color
-  */
+   ** Customize the progress-bar color
+   */
   loading: { color: '#fff' },
   /*
-  ** Global CSS
-  */
-  css: [
-    'element-ui/lib/theme-chalk/index.css',
-    'element-ui/lib/theme-chalk/display.css',
-    'mavon-editor/dist/css/index.css'
-  ],
+   ** Global CSS
+   */
+  css: ['element-ui/lib/theme-chalk/index.css', 'element-ui/lib/theme-chalk/display.css', 'mavon-editor/dist/css/index.css'],
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     '@/plugins/element-ui',
     '@/api/note.js',
     '@/api/common.js',
     '@/api/life.js',
     '@/api/user.js',
-    { src: '@/plugins/mavon-editor', mode: 'client' },
+    { src: '@/plugins/mavon-editor', mode: 'client' }
   ],
   /*
-  ** Nuxt.js dev-modules
-  */
+   ** Nuxt.js dev-modules
+   */
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
-    '@nuxtjs/tailwindcss',
+    '@nuxtjs/tailwindcss'
   ],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/axios',
-    'cookie-universal-nuxt'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/axios', 'cookie-universal-nuxt'],
   axios: {
     proxy: true,
-    prefix:'/api'
+    prefix: '/api'
   },
   proxy: {
-    '/api':{
+    '/api': {
       target: 'https://www.fastmock.site/mock/7bd9f6a21e18a9c791fd338a1a007d59/baldbear-blog',
-      pathRewrite: {'^/api': ''}
+      pathRewrite: { '^/api': '' }
     }
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    transpile:[/^element-ui/],
+    transpile: [/^element-ui/],
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
 }
