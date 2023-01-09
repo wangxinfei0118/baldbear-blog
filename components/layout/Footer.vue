9a298004279611d914d86bc1fc5df8d0edb53d38
wxf <1019072635@qq.com>
2023-01-09 23:37:05 +0800

    fix: 修改底部信息

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="bg-footer-bg h-20 flex justify-center items-center text-small-x">
-    <div class="mr-2">
-      <a href="https://beian.miit.gov.cn/" target="_blank">冀ICP备2022030887号</a>
+  <div class="bg-footer-bg h-20 flex justify-center items-center text-small-x text-home-title">
+    <div class="mr-2">Copyright © 2022-2023 baldbear.cn All Rights Reserved.</div>
+    <div>
+      <a href="https://beian.miit.gov.cn/" target="_blank">备案号：冀ICP备2022030887号</a>
     </div>
-    <div>联系方式：wangxinfei0118@163.com</div>
   </div>
 </template>
 
