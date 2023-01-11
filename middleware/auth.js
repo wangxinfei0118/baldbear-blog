ca0842b0506c925e4418fa0751093c9d77c37cda
wxf <1019072635@qq.com>
2023-01-11 22:14:22 +0800

    fix: 修改跳转登录判断条件

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 export default ({ store, route, redirect, req }) => {
-  if (!store || !store.state.userInfo) {
+  if (!store || !store.state.userInfo || !store.state.accessToken) {
     // 通过 req.headers.host 在生产环境获取域名
     const redirectURL = 'http://' + req.headers.host + route.path
     redirect(`${process.env.authURL}?redirectURL=${redirectURL}`)
