3036e83eb6c0d18c4dae45179b75086df1484bba
wxf <1019072635@qq.com>
2023-01-11 21:00:50 +0800

    feat: 实现首次加载loading动画

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,49 @@
 <template>
   <div>
-    <!--头部-->
-    <Header></Header>
-    <!--主体-->
-    <div class="main">
-      <nuxt />
+    <Loading v-if="isLoading" :isFinish="isFinish"></Loading>
+    <div>
+      <!--头部-->
+      <Header></Header>
+      <!--主体-->
+      <div class="main">
+        <nuxt />
+      </div>
+      <!--尾部-->
+      <Footer v-if="$route.path === '/'"></Footer>
+      <!--回到顶部-->
+      <el-backtop :bottom="80"></el-backtop>
     </div>
-    <!--尾部-->
-    <Footer v-if="$route.path === '/'"></Footer>
-    <!--回到顶部-->
-    <el-backtop :bottom="80"></el-backtop>
   </div>
 </template>
 
 <style></style>
 <script>
-import Footer from '~/components/layout/Footer'
-import Header from '~/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import Header from '@/components/layout/Header'
+import Loading from '@/components/layout/Loading'
 export default {
-  components: { Header, Footer }
+  components: { Loading, Header, Footer },
+  data() {
+    return {
+      isLoading: true,
+      isFinish: false
+    }
+  },
+  created() {
+    let that = this
+    if (process.client) {
+      document.onreadystatechange = function () {
+        if (document.readyState === 'complete') {
+          setTimeout(function () {
+            that.isFinish = true
+            setTimeout(() => {
+              that.isLoading = false
+            }, 1000)
+          }, 600)
+        }
+      }
+    }
+  }
 }
 </script>
 <style scoped>
