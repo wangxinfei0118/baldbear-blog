d290583a65c90296414d9c28561beed9a0684547
wxf <1019072635@qq.com>
2023-01-17 00:46:00 +0800

    feat: elementui按需引入

--- plugins/element-ui.js
+++ plugins/element-ui.js
@@ -1,3 +1,81 @@
-import Vue from 'vue';
-import ElementUI from 'element-ui';
-Vue.use(ElementUI);
+import Vue from 'vue'
+import {
+  Button,
+  Input,
+  Row,
+  Col,
+  Icon,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  MenuItem,
+  MenuItemGroup,
+  Submenu,
+  Radio,
+  Card,
+  MessageBox,
+  Backtop,
+  Divider,
+  Timeline,
+  TimelineItem,
+  Collapse,
+  CollapseItem,
+  Upload,
+  Tree,
+  Tag,
+  Form,
+  FormItem,
+  Tooltip,
+  Table,
+  TableColumn,
+  RadioGroup,
+  Avatar,
+  Empty,
+  Popover
+} from 'element-ui'
+
+const components = [
+  Button,
+  Input,
+  Row,
+  Col,
+  Icon,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Menu,
+  MenuItem,
+  MenuItemGroup,
+  Submenu,
+  Radio,
+  Card,
+  MessageBox,
+  Backtop,
+  Divider,
+  Timeline,
+  TimelineItem,
+  Collapse,
+  CollapseItem,
+  Upload,
+  Tree,
+  Tag,
+  Form,
+  FormItem,
+  Tooltip,
+  Table,
+  TableColumn,
+  RadioGroup,
+  Avatar,
+  Empty,
+  Popover
+]
+const Element = {
+  install(Vue) {
+    components.forEach((component) => {
+      Vue.component(component.name, component)
+    })
+  }
+}
+Vue.prototype.$mb = MessageBox
+Vue.use(Element)
