1e90c07ebd80fa1912b26e7d5c358a39d841ef40
wxf <1019072635@qq.com>
2023-02-01 00:20:14 +0800

    fix: 参数修改，移除废弃表单验证规则

--- components/user/Password.vue
+++ components/user/Password.vue
@@ -3,13 +3,13 @@
     <el-row type="flex" justify="center" style="padding-top: 30px">
       <el-form :rules="rules" ref="formData" :model="formData" label-width="100px" style="width: 400px" status-icon>
         <el-form-item label="原密码：" prop="oldPassword">
-          <el-input type="password" placeholder="请输入原密码" v-model="formData.oldPassword"></el-input>
+          <el-input type="password" placeholder="请输入原密码" v-model="formData.oldPwd"></el-input>
         </el-form-item>
         <el-form-item label="新密码：" prop="newPassword">
-          <el-input type="password" placeholder="请输入新密码" v-model="formData.newPassword"></el-input>
+          <el-input type="password" placeholder="请输入新密码" v-model="formData.newPwd"></el-input>
         </el-form-item>
         <el-form-item label="确认密码：" prop="repPassword">
-          <el-input type="password" placeholder="请输入确认密码" v-model="formData.repPassword"></el-input>
+          <el-input type="password" placeholder="请输入确认密码" v-model="formData.repPwd"></el-input>
         </el-form-item>
         <el-form-item align="center">
           <el-button size="mini" type="primary" :loading="loading" @click="submitForm('formData')">确 定</el-button>
@@ -30,9 +30,9 @@ export default {
       default: () => {
         return {
           userId: '',
-          oldPassword: '',
-          newPassword: '',
-          repPassword: ''
+          oldPwd: '',
+          newPwd: '',
+          repPwd: ''
         }
       }
     }
@@ -49,26 +49,6 @@ export default {
     }
   },
   data() {
-    const validateOldPassword = (rule, value, callback) => {
-      if (!value || value.length < 6) {
-        callback(new Error('请输入正确的原密码'))
-      }
-      // 发送请求校验原密码
-      const data = { userId: this.$store.state.userInfo.uid, oldPassword: value }
-      this.$checkOldPassword(data)
-        .then((response) => {
-          // 校验通过
-          if (response.code === 20000) {
-            callback()
-          } else {
-            callback(new Error(response.message))
-          }
-        })
-        .catch((error) => {
-          // 校验失败
-          callback(new Error('原密码校验异常，请稍后重试'))
-        })
-    }
     // 校验新密码
     const validatePassword = (rule, value, callback) => {
       if (value.length < 6) {
@@ -79,7 +59,7 @@ export default {
     }
     // 校验确认密码是否一致
     const validateRepPassword = (rule, value, callback) => {
-      if (value !== this.formData.newPassword) {
+      if (value !== this.formData.newPwd) {
         callback(new Error('两次输入的密码不一致'))
       } else {
         callback()
@@ -87,15 +67,12 @@ export default {
     }
     return {
       rules: {
-        oldPassword: [
-          { required: true, message: '原密码不能为空', trigger: 'blur' },
-          { validator: validateOldPassword, trigger: 'blur' }
-        ],
-        newPassword: [
+        oldPwd: [{ required: true, message: '原密码不能为空', trigger: 'blur' }],
+        newPwd: [
           { required: true, message: '新密码不能为空', trigger: 'blur' },
           { validator: validatePassword, trigger: 'blur' }
         ],
-        repPassword: [
+        rePwd: [
           { required: true, message: '确认密码不能为空', trigger: 'blur' },
           { validator: validateRepPassword, trigger: ['change', 'blur'] }
         ]
