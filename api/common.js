b800c59d3dad0237b07278cf724014a9af6980a5
wxf <1019072635@qq.com>
2023-02-06 03:26:18 +0800

    修改上传文件时的请求头

--- api/common.js
+++ api/common.js
@@ -1,6 +1,15 @@
 export default ({ $axios }, inject) => {
   // 上传图片
-  inject('uploadImg', (data) => $axios.$post(`/file/image/upload`, data))
+  inject('uploadImg', (data) =>
+    $axios({
+      url: `/file/image/upload`,
+      method: 'post',
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      },
+      data
+    })
+  )
   // 删除图片
   inject('deleteImg', (fileUrl) => $axios.$delete(`/file/image/delete`, { params: { fileUrl } }))
 }
