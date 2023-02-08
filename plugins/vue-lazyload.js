54d70de9300c9157a73ee6abd51b3f298e065276
wxf <1019072635@qq.com>
2023-02-09 00:47:50 +0800

    更新vue-lazyload.js

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import VueLazyLoad from 'vue-lazyload'
 
-//加载中的图片
+// 加载中的图片
 const loadimage = require('@/assets/img/loading.gif')
-//加载图片出错展现的图片
+// 加载图片出错展现的图片
 const errorimage = require('@/assets/img/logo.png')
 
 Vue.use(VueLazyLoad, {
-  preLoad: 1.33, //预加载高度比例
   loading: loadimage,
   error: errorimage,
-  attempt: 2, //尝试计数次数
-  throttleWait: 500 //节流等待时间(通过调节此值可以查看loading的效果，根据实际情况调节)
+  attempt: 1 // 加载失败尝试重新加载次数
 })
