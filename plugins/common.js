5332afebde2cb09c30723bcc880dea0afaf6e4c8
wxf <1019072635@qq.com>
2023-02-09 16:22:07 +0800

    将默认头像添加到vue实例

--- plugins/common.js
+++ plugins/common.js
@@ -1,6 +1,10 @@
+import Vue from 'vue'
+
 export default (content, inject) => {
   // 处理数据转义问题
   inject('handleEscape', (str) => {
     return str.replace(/\\n/g, '\n').replace(/\\"/g, '"')
   })
 }
+
+Vue.prototype.$avatar = require('@/assets/img/avatar.svg')
